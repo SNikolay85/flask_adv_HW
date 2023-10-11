3a7b97f352e8f87e3f308a5f1b53e8297415fdbe
nikolay_speshilov <sny_85@mail.ru>
2023-10-11 16:13:55 +0400

    now

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 
-# response = requests.post('http://127.0.0.1:8000/advertisements/',
-#             json={'header': 'adv_2', 'description': 'text_1', 'user': 'user_1'})
+response = requests.post('http://127.0.0.1:8000/advertisements/',
+            json={'header': 'adv_2', 'description': 'text_1', 'user': 'user_2'})
 
 
 # response = requests.patch('http://127.0.0.1:8000/advertisements/user_2/2',
@@ -11,21 +11,21 @@ import requests
 
 # response = requests.patch('http://127.0.0.1:8000/advertisements/3',
 #                          json={'description': 'adv_1 text_2'})
+print(response.status_code)
+print(response.text)
+
+# response = requests.delete("http://127.0.0.1:8000/advertisements/user_2/2")
+#
 # print(response.status_code)
 # print(response.text)
 
-response = requests.delete("http://127.0.0.1:8000/advertisements/user_2/2")
-
-print(response.status_code)
-print(response.text)
-
-response = requests.get("http://127.0.0.1:8000/advertisements/1")
-
-print(response.status_code)
-print(response.text)
-
-
-response = requests.get("http://127.0.0.1:8000/advertisements/2")
-
-print(response.status_code)
-print(response.text)
+# response = requests.get("http://127.0.0.1:8000/advertisements/1")
+#
+# print(response.status_code)
+# print(response.text)
+#
+#
+# response = requests.get("http://127.0.0.1:8000/advertisements/2")
+#
+# print(response.status_code)
+# print(response.text)
